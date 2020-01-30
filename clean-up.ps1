3dc1521cbb54621d025a6303f0b85f85d69deacf
fergusoa <alex.ferguson@canada.ca>
2020-01-30 12:12:01 -0500

    adding additional paths

--- clean-up.ps1
+++ clean-up.ps1
@@ -1,6 +1,7 @@
 
 
-
+Remove-Item -path .\Basic_*\HTAP-sim-* -Recurse
+Remove-Item -path .\Basic_*\HTAP-work-* -Recurse
 Remove-Item -path .\Basic_*\*.csv
 Remove-Item -path .\Basic_*\*.out
 Remove-Item -path .\Basic_*\*.txt
@@ -8,3 +9,19 @@ Remove-Item -path .\Basic_*\*.resume
 Remove-Item -path .\Basic_*\HTAP-prm-output.json
 
 
+Remove-Item -path .\Intermediate_*\HTAP-sim-* -Recurse
+Remove-Item -path .\Intermediate_*\HTAP-work-* -Recurse
+Remove-Item -path .\Intermediate_*\*.csv
+Remove-Item -path .\Intermediate_*\*.out
+Remove-Item -path .\Intermediate_*\*.txt
+Remove-Item -path .\Intermediate_*\*.resume 
+Remove-Item -path .\Intermediate_*\HTAP-prm-output.json
+
+
+Remove-Item -path .\Exercise_*\HTAP-sim-* -Recurse
+Remove-Item -path .\Exercise_*\HTAP-work-* -Recurse
+Remove-Item -path .\Exercise_*\*.csv
+Remove-Item -path .\Exercise_*\*.out
+Remove-Item -path .\Exercise_*\*.txt
+Remove-Item -path .\Exercise_*\*.resume 
+Remove-Item -path .\Exercise_*\HTAP-prm-output.json
